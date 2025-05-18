1f0e5b43a508b82db02e775081b4bc55d0807bd4
taberkkaya <taberkkaya@gmail.com>
2025-05-18 22:14:38 +0300

    Add project files.

---
+++ README.md
@@ -0,0 +1,39 @@
+<p align="center">
+    <img src="icon.jpg" alt="Logo" width="200" />
+    <br>
+    <h1 align="center">ResultKit</h1>
+</p>
+
+**Clean, lightweight, and extensible Result type implementation for .NET**  
+Simplifies error handling and functional programming patterns by encapsulating success and failure states consistently.
+
+## 🚀 Features
+
+- Provides a clear and consistent way to represent success and failure states
+- Supports chaining and functional composition for better error handling
+- Minimal dependencies to keep the library lightweight and performant
+- Extensible design to customize behavior as needed
+- Suitable for both small projects and scalable enterprise applications
+- Open source and MIT licensed for maximum flexibility
+
+## 📦 Installation
+
+Install RepositoryKit via NuGet Package Manager Console:
+
+```bash
+Install-Package ResultKit
+```
+
+Or using the .NET CLI:
+
+```
+dotnet add package ResultKit
+```
+
+## 📝 Notes
+
+_ResultKit started as a project inspired by and built upon the foundation of Taner Saydam's [TS.Result](https://github.com/TanerSaydam/TS.Result) repository. It aims to provide a clean, lightweight, and extensible Result type implementation tailored for modern .NET development. Future versions will focus on adding new features and improving customization options._
+
+## ⚖️ License
+
+**RepositoryKit is licensed under the [MIT License](https://github.com/taberkkaya/ResultKit/blob/master/LICENSE.txt).**
---
+++ ResultKit/Result.cs
@@ -0,0 +1,86 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ResultKit;
+
+public sealed class Result<T>
+{
+    [JsonPropertyName("data")]
+    public T? Data { get; set; }
+    [JsonPropertyName("errorMessages")]
+    public List<string>? ErrorMessages { get; set; }
+    [JsonPropertyName("isSuccessful")]
+    public bool IsSuccessful { get; set; } = true;
+
+    [JsonPropertyName("statusCode")]
+    public int StatusCode { get; set; } = (int)HttpStatusCode.OK;
+
+    [JsonConstructor]
+    public Result()
+    {
+    }
+
+    public Result(T data)
+    {
+        Data = data;
+    }
+
+    public Result(int statusCode, List<string> errorMessages)
+    {
+        IsSuccessful = false;
+        StatusCode = statusCode;
+        ErrorMessages = errorMessages;
+    }
+
+    public Result(int statusCode, string errorMessage)
+    {
+        IsSuccessful = false;
+        StatusCode = statusCode;
+        ErrorMessages = new() { errorMessage };
+    }
+
+    public static implicit operator Result<T>(T data)
+    {
+        return new(data);
+    }
+
+    public static implicit operator Result<T>((int statusCode, List<string> errorMessages) parameters)
+    {
+        return new(parameters.statusCode, parameters.errorMessages);
+    }
+
+    public static implicit operator Result<T>((int statusCode, string errorMessage) parameters)
+    {
+        return new(parameters.statusCode, parameters.errorMessage);
+    }
+    public static Result<T> Succeed(T data)
+    {
+        return new(data);
+    }
+
+    public static Result<T> Failure(int statusCode, List<string> errorMessages)
+    {
+        return new(statusCode, errorMessages);
+    }
+
+    public static Result<T> Failure(int statusCode, string errorMessage)
+    {
+        return new(statusCode, errorMessage);
+    }
+
+    public static Result<T> Failure(string errorMessage)
+    {
+        return new(500, errorMessage);
+    }
+
+    public static Result<T> Failure(List<string> errorMessages)
+    {
+        return new(500, errorMessages);
+    }
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
